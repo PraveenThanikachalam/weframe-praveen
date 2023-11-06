ec7fdf450aa701466f0cbe471e306491ea3bc010
Ayush Bathre <ayushbathre3101@gmail.com>
2023-11-06 13:10:24 +0530

    swiper js added

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
+
 import FaqComponent from '@/components/FaqComponent';
 import StoryCard from '@/components/StoryCard';
 import TechStack from '@/components/TechStack';
 
+
 import Image from 'next/image';
+import ClientComponent from '@/components/ClientComponent';
 
 export default function Home() {
+
   return (
     <main>
       <div className="pattern sec1 lg:h-[115vh] h-[125vh] md:h-[80vh]  z-30 text-center text-white w-full flex flex-col items-center justify-end ">
@@ -240,129 +244,9 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="w-full mt-36  flex items-center justify-center">
-        <div className="w-[80%] flex flex-col lg:flex-row text-center lg:text-left items-center justify-between">
-          <div>
-            <h1 className="text-4xl text-white font-bold">Hear from Clients</h1>
-          </div>
-          <div className="flex gap-3 mt-4 lg:mt-0 text-white">
-            <div className="w-12 h-12 rounded-full flex items-center justify-center border border-white">
-              <i className="ri-arrow-left-line"></i>
-            </div>
-            <div className="w-12 h-12 rounded-full flex items-center justify-center border border-white">
-              <i className="ri-arrow-right-line"></i>
-            </div>
-          </div>
-        </div>
-      </div>
 
-      <div className="w-full mt-12 flex items-center justify-end">
-        <div className="clientSay w-[95%] lg:w-[90%]  overflow-scroll  flex flex-col items-start justify-start">
-          <div className=" flex items-start justify-start overflow-scroll  gap-5">
-            <div className="lg:w-[700px] w-[400px] lg:h-96 h-56 relative flex items-center justify-center rounded-xl">
-              <img
-                src="/assets/client/client.png"
-                className="h-full w-full rounded-xl"
-                alt=""
-              />
-              <div className="w-[90%] absolute bottom-3 flex items-center justify-between">
-                <div className="  w-56 flex items-center justify-center">
-                  <div>
-                    <img src="/assets/client/david.png" alt="" />
-                  </div>
-                  <div className="ml-3">
-                    <p className="text-sm font-bold text-white">
-                      David Goggins
-                    </p>
-                    <p className="text-xs text-gray-200">
-                      CEO, Fit India Movement
-                    </p>
-                  </div>
-                </div>
-                <div>
-                  <img src="/assets/client/pause.svg" alt="" />
-                </div>
-              </div>
-              <div>
-                <p className="absolute top-6 right-6 text-white text-xs font-bold">
-                  View Project <i className="ri-arrow-right-line"></i>{' '}
-                </p>
-              </div>
-            </div>
-            <div className="lg:w-[700px] w-[400px] lg:h-96 h-56 relative flex items-center justify-center rounded-xl">
-              <img
-                src="/assets/client/client.png"
-                className="h-full w-full rounded-xl"
-                alt=""
-              />
-              <div className="w-full h-full bg-black absolute z-10 rounded-xl opacity-90 flex items-center text-xs lg:text-sm justify-center text-white p-16">
-                Working with WeFrameTech on our Ownerpreneur project has been
-                nothing short of exceptional. Their innovative design and
-                flawless development not only met but exceeded our expectations.
-                The result is a cutting-edge platform that beautifully
-                encapsulates our vision. Their commitment to excellence and
-                dedication to our success made this collaboration truly
-                transformative. We highly recommend WeFrameTech to anyone
-                looking to turn their entrepreneurial dreams into reality.
-              </div>
-              <div className="w-[90%] absolute z-20 bottom-3 flex items-center justify-between">
-                <div className="  w-56 flex items-center justify-center">
-                  <div>
-                    <img src="/assets/client/david.png" alt="" />
-                  </div>
-                  <div className="ml-3">
-                    <p className="text-sm font-bold text-white">
-                      David Goggins
-                    </p>
-                    <p className="text-xs text-gray-200">
-                      CEO, Fit India Movement
-                    </p>
-                  </div>
-                </div>
-                <div>
-                  <img src="/assets/client/play.svg" alt="" />
-                </div>
-              </div>
-              <div>
-                <p className="absolute z-20 top-6 right-6 text-white text-xs font-bold">
-                  View Project <i className="ri-arrow-right-line"></i>
-                </p>
-              </div>
-            </div>
-            <div className="lg:w-[700px] w-[400px] lg:h-96 h-56 relative flex items-center justify-center rounded-xl">
-              <div className="w-full h-full bg-black absolute z-10 rounded-xl text-xs lg:text-sm  flex items-center justify-center text-white p-16">
-                Working with WeFrameTech on our Ownerpreneur project has been
-                nothing short of exceptional. Their innovative design and
-                flawless development not only met but exceeded our expectations.
-                The result is a cutting-edge platform that beautifully
-                encapsulates our vision. Their commitment to excellence and
-                dedication to our success made this collaboration truly
-                transformative. We highly recommend WeFrameTech to anyone
-                looking to turn their entrepreneurial dreams into reality.
-              </div>
-              <div className="w-[90%] absolute z-20 bottom-3 flex items-center justify-between">
-                <div className="  w-56 flex items-center justify-center">
-                  <div>
-                    <img src="/assets/client/david.png" alt="" />
-                  </div>
-                  <div className="ml-3">
-                    <p className="text-sm font-bold text-white">
-                      David Goggins
-                    </p>
-                    <p className="text-xs text-gray-200">
-                      CEO, Fit India Movement
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div>
-                <p className="absolute z-20 top-6 right-6 text-white text-xs font-bold">
-                  View Project <i className="ri-arrow-right-line"></i>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className='w-full mt-32'>
+        <ClientComponent/>
       </div>
 
       <div className=" clients mt-48 w-full flex flex-col items-center justify-center">
