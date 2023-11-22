654f845f8990c5e6020ae613f7526fa68d6f8df8
Ayush Bathre <ayushbathre3101@gmail.com>
2023-11-22 14:46:50 +0530

    faqComponent changed

--- components/HomePage/FaqComponent.jsx
+++ components/HomePage/FaqComponent.jsx
@@ -6,7 +6,37 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from '@/components/ui/accordion';
-import Image from 'next/image';
+
+const arr = [
+  {
+    title: 'What is Headless Commerce and Jamstack Development?',
+    desc: '  Jamstack (JavaScript, APIs, and Markup) is a web development architecture that emphasizes the separation of content from presentation. It promotes faster loading times, improved security, and scalability by relying on APIs and pre-rendering content. At our agency, we specialize in combining the power of headless commerce and Jamstack development to create high-performance, flexible, and scalable e-commerce solutions for our clients.',
+  },
+  {
+    title:
+      '   What are the Benefits of Headless Commerce and Jamstack Development?',
+    desc: '  Jamstack (JavaScript, APIs, and Markup) is a web development architecture that emphasizes the separation of content from presentation. It promotes faster loading times, improved security, and scalability by relying on APIs and pre-rendering content. At our agency, we specialize in combining the power of headless commerce and Jamstack development to create high-performance, flexible, and scalable e-commerce solutions for our clients.',
+  },
+  {
+    title:
+      'Can I Migrate my Existing E-Commerce Platform to a Headless Commerce Solution?',
+    desc: '  Jamstack (JavaScript, APIs, and Markup) is a web development architecture that emphasizes the separation of content from presentation. It promotes faster loading times, improved security, and scalability by relying on APIs and pre-rendering content. At our agency, we specialize in combining the power of headless commerce and Jamstack development to create high-performance, flexible, and scalable e-commerce solutions for our clients.',
+  },
+  {
+    title:
+      ' How Do You Ensure Mobile Responsiveness in Headless Commerce Solution?',
+    desc: '  Jamstack (JavaScript, APIs, and Markup) is a web development architecture that emphasizes the separation of content from presentation. It promotes faster loading times, improved security, and scalability by relying on APIs and pre-rendering content. At our agency, we specialize in combining the power of headless commerce and Jamstack development to create high-performance, flexible, and scalable e-commerce solutions for our clients.',
+  },
+  {
+    title:
+      ' Can I Integrate Third-Party Services and APIs with a Headless Commerce System?',
+    desc: '  Jamstack (JavaScript, APIs, and Markup) is a web development architecture that emphasizes the separation of content from presentation. It promotes faster loading times, improved security, and scalability by relying on APIs and pre-rendering content. At our agency, we specialize in combining the power of headless commerce and Jamstack development to create high-performance, flexible, and scalable e-commerce solutions for our clients.',
+  },
+  {
+    title: 'How Do You Ensure the Security of Headless Commerce Solution?',
+    desc: '  Jamstack (JavaScript, APIs, and Markup) is a web development architecture that emphasizes the separation of content from presentation. It promotes faster loading times, improved security, and scalability by relying on APIs and pre-rendering content. At our agency, we specialize in combining the power of headless commerce and Jamstack development to create high-performance, flexible, and scalable e-commerce solutions for our clients.',
+  },
+];
 const FaqComponent = () => {
   return (
     <div className="faq w-full flex my-48 items-center text-center justify-center flex-col">
@@ -17,94 +47,20 @@ const FaqComponent = () => {
       </div>
       <div className="lg:w-[70%] w-[80%] mt-16">
         <Accordion type="single" collapsible className="w-full text-[#ebebeb]">
-          <AccordionItem value="item-1" className="borderGrd px-4 mt-3">
-            <AccordionTrigger>
-              What is Headless Commerce and Jamstack Development?
-            </AccordionTrigger>
-            <AccordionContent className="text-[#999999] text-left">
-              Jamstack (JavaScript, APIs, and Markup) is a web development
-              architecture that emphasizes the separation of content from
-              presentation. It promotes faster loading times, improved security,
-              and scalability by relying on APIs and pre-rendering content. At
-              our agency, we specialize in combining the power of headless
-              commerce and Jamstack development to create high-performance,
-              flexible, and scalable e-commerce solutions for our clients.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-2" className="borderGrd px-4 mt-3">
-            <AccordionTrigger>
-              What are the Benefits of Headless Commerce and Jamstack
-              Development?
-            </AccordionTrigger>
-            <AccordionContent className="text-[#999999] text-left">
-              Jamstack (JavaScript, APIs, and Markup) is a web development
-              architecture that emphasizes the separation of content from
-              presentation. It promotes faster loading times, improved security,
-              and scalability by relying on APIs and pre-rendering content. At
-              our agency, we specialize in combining the power of headless
-              commerce and Jamstack development to create high-performance,
-              flexible, and scalable e-commerce solutions for our clients.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-3" className="borderGrd px-4 mt-3">
-            <AccordionTrigger>
-              Can I Migrate my Existing E-Commerce Platform to a Headless
-              Commerce Solution?
-            </AccordionTrigger>
-            <AccordionContent className="text-[#999999] text-left">
-              Jamstack (JavaScript, APIs, and Markup) is a web development
-              architecture that emphasizes the separation of content from
-              presentation. It promotes faster loading times, improved security,
-              and scalability by relying on APIs and pre-rendering content. At
-              our agency, we specialize in combining the power of headless
-              commerce and Jamstack development to create high-performance,
-              flexible, and scalable e-commerce solutions for our clients.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-4" className="borderGrd px-4 mt-3">
-            <AccordionTrigger>
-              How Do You Ensure Mobile Responsiveness in Headless Commerce
-              Solution?
-            </AccordionTrigger>
-            <AccordionContent className="text-[#999999] text-left">
-              Jamstack (JavaScript, APIs, and Markup) is a web development
-              architecture that emphasizes the separation of content from
-              presentation. It promotes faster loading times, improved security,
-              and scalability by relying on APIs and pre-rendering content. At
-              our agency, we specialize in combining the power of headless
-              commerce and Jamstack development to create high-performance,
-              flexible, and scalable e-commerce solutions for our clients.
-            </AccordionContent>
-          </AccordionItem>
-           <AccordionItem value="item-5" className="borderGrd px-4 mt-3">
-            <AccordionTrigger>
-              Can I Integrate Third-Party Services and APIs with a Headless
-              Commerce System?
-            </AccordionTrigger>
-            <AccordionContent className="text-[#999999] text-left">
-              Jamstack (JavaScript, APIs, and Markup) is a web development
-              architecture that emphasizes the separation of content from
-              presentation. It promotes faster loading times, improved security,
-              and scalability by relying on APIs and pre-rendering content. At
-              our agency, we specialize in combining the power of headless
-              commerce and Jamstack development to create high-performance,
-              flexible, and scalable e-commerce solutions for our clients.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-6" className="borderGrd px-4 mt-3">
-            <AccordionTrigger>
-              How Do You Ensure the Security of Headless Commerce Solution?
-            </AccordionTrigger>
-            <AccordionContent className="text-[#999999] text-left">
-              Jamstack (JavaScript, APIs, and Markup) is a web development
-              architecture that emphasizes the separation of content from
-              presentation. It promotes faster loading times, improved security,
-              and scalability by relying on APIs and pre-rendering content. At
-              our agency, we specialize in combining the power of headless
-              commerce and Jamstack development to create high-performance,
-              flexible, and scalable e-commerce solutions for our clients.
-            </AccordionContent>
-          </AccordionItem>
+          {arr.map((item, index) => {
+            return (
+              <AccordionItem
+                value={`item-${index}`}
+                className="borderGrd px-4 mt-3"
+                key={index}
+              >
+                <AccordionTrigger>{item.title}</AccordionTrigger>
+                <AccordionContent className="text-[#999999] text-left">
+                  {item.desc}
+                </AccordionContent>
+              </AccordionItem>
+            );
+          })}
         </Accordion>
       </div>
     </div>
