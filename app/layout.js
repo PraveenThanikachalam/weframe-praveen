ccc78d09f186a481b9398b5e6b353f47668bbc5d
Ayush Bathre <ayushbathre3101@gmail.com>
2024-03-07 18:51:20 +0530

    icons added

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,27 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${oceanwide.variable}`}>
-      <link rel="icon" href="/favicon.ico" sizes="96x96" type="image/x-icon" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
       <body className={`${font.className} overflow-y-scroll no-scrollbar`}>
         <NextTopLoader
           color="#00e6e6"
