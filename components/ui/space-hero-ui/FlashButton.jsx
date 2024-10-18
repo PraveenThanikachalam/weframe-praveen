38cf1679bd6a0de7da43d341bc42f4b5a2a4b62a
PraveenThanikachalam <praveenthanikachalam408@gmail.com>
2024-10-19 00:13:46 +0530

    hyperlinked - Book a meet btn

--- components/ui/space-hero-ui/FlashButton.jsx
+++ components/ui/space-hero-ui/FlashButton.jsx
@@ -1,12 +1,17 @@
+import Link from "next/link";
+
 export default function FlashButton() {
 	return (
-		<div className="animated-border-box w-[200px] md:w-[250px] h-[65px] translate-y-48 md:translate-y-48">
+		<Link
+			href={"https://calendly.com/vipuluthaiah/weframe-tech?month=2024-10"}
+			className="animated-border-box w-[200px] md:w-[250px] h-[65px] translate-y-48 md:translate-y-48"
+		>
 			<div className="flashlight" />
 			<div className="absolute inset-0 flex items-center justify-center z-20">
 				<p className="text-white text-md md:text-lg font-bold">
 					Book a Meeting
 				</p>
 			</div>
-		</div>
+		</Link>
 	);
 }
